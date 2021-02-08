5d548d29942cd2b2f3448a35f5aac0ae48eb8876
jameygleason <james.p.gleason@gmail.com>
2021-02-08 18:37:34 -0500

    built error handler component

--- sapper/src/routes/handle-errors.svelte
+++ sapper/src/routes/handle-errors.svelte
@@ -3,8 +3,6 @@
   import { client, gql } from "../graphql.js";
   import { safeJsonParse } from "../utils/safeJsonParse.js";
 
-  let dump;
-
   onMount(() => {
     queryError();
   });
@@ -19,21 +17,28 @@
     const [data, error] = await client.query(HANDLE_ERRORS_QUERY);
 
     if (error) {
-      console.error(error);
-      let [json] = safeJsonParse(error.message);
-      if (json.constructor === String) {
-        json = {
-          alert: json,
-        };
-      }
-      dump = JSON.stringify(json, null, 2);
+      errorStore(error);
       return;
     }
 
     console.log(data);
   }
-</script>
 
-<pre>
-  {dump}
-</pre>
+  // This should be the error store set errors function
+  function errorStore(error) {
+    console.error(error.name);
+    let [json] = safeJsonParse(error.message);
+    if (json.constructor === String) {
+      json = {
+        minusqlCatchBlockError: json,
+      };
+    }
+    if (json.constructor === Object) {
+      for (const val of Object.values(json)) {
+        console.error(val);
+      }
+    } else {
+      console.error(error);
+    }
+  }
+</script>
