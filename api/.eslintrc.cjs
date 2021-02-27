93c1fe9053665aaf6bbea866137b4f94268f3868
jameygleason <james.p.gleason@gmail.com>
2021-02-27 18:08:26 -0500

    fixed bug in eslint rule for import extensions

--- api/.eslintrc.cjs
+++ api/.eslintrc.cjs
@@ -56,11 +56,10 @@ module.exports = {
     "import/export": "error",
     "import/extensions": [
       "error",
-      "always",
+      "ignorePackages",
       {
         js: "always",
         ts: "never", // Should be set to always. Prevented by Typescript error.
-        ignorePackages: true,
       },
     ],
     "import/first": "error",
