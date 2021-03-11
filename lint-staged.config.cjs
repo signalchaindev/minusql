f6712302b51a4a8ea83f2fa1480faa78ea1375fb
jameygleason <james.p.gleason@gmail.com>
2021-03-11 10:09:56 -0500

    added test query

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-commonjs */
 module.exports = {
   "*.(js|ts|json|graphql|svelte)": ["eslint"],
   // '*.+(js|ts|json|css|scss|md|graphql)': ['prettier --write', 'git add'],
--- sapper-ts/src/routes/_layout.svelte
+++ sapper-ts/src/routes/_layout.svelte
@@ -1,7 +1,21 @@
 <script lang="ts">
+  import { onMount } from "svelte"
+  import { client, gql } from "../graphql.js"
   import Nav from "../components/Nav.svelte"
 
   export let segment: string
+
+  const TEST_QUERY = gql`
+    query TEST_QUERY {
+      testConnection
+    }
+  `
+
+  onMount(async () => {
+    const [data, error] = await client.query(TEST_QUERY)
+    console.log("data:", data)
+    console.log("error:", error)
+  })
 </script>
 
 <Nav {segment} />
